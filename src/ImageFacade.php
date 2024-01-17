b38a81be35da2f253b675f96df1a9ee97357f479
Oliver Vogel <oliver@olivervogel.com>
2024-01-17 09:05:07 +0100

    Add Laravel ServiceProvider & Facade

---
+++ src/ImageFacade.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Intervention\Image\Laravel;
+
+use Illuminate\Support\Facades\Facade;
+
+class ImageFacade extends Facade
+{
+    /**
+     * @method \Intervention\Image\ImageManager read(mixed $input, string|array|DecoderInterface $decoders = [])
+     * @method \Intervention\Image\ImageManager create(int $width, int $height)
+     * @method \Intervention\Image\ImageManager animate(callable $callback)
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'image';
+    }
+}
---
+++ src/ServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Intervention\Image\Laravel;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Intervention\Image\ImageManager;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Binding name of the service container
+     */
+    protected const BINDING = 'image';
+
+    /**
+     * Bootstrap application events
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../../config/image.php' => config_path($this::BINDING . '.php')
+        ]);
+    }
+
+    /**
+     * Register the image service
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../../config/image.php',
+            $this::BINDING
+        );
+
+        $this->app->singleton($this::BINDING, function ($app) {
+            return new ImageManager(config('image.driver'));
+        });
+    }
+}
