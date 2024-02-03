ed7a0a45bf791fd53a5551cc69d491e8abbe5b05
Diamond Mubaarak <diamondobama@gmail.com>
2024-02-03 21:35:23 +0200

    Moves methods to class-level and makes them static

--- src/Facades/Image.php
+++ src/Facades/Image.php
@@ -6,14 +6,14 @@ namespace Intervention\Image\Laravel\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Intervention\Image\ImageManager read(mixed $input, string|array|\Intervention\Image\Interfaces\DecoderInterface $decoders = [])
+ * @method static \Intervention\Image\ImageManager create(int $width, int $height)
+ * @method static \Intervention\Image\ImageManager animate(callable $callback)
+ */
 class Image extends Facade
 {
-    /**
-     * @method \Intervention\Image\ImageManager read(mixed $input, string|array|DecoderInterface $decoders = [])
-     * @method \Intervention\Image\ImageManager create(int $width, int $height)
-     * @method \Intervention\Image\ImageManager animate(callable $callback)
-     */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'image';
     }
