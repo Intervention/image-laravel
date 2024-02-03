e5cce7288a248cb9849a4fb21e7cf5fadd4675c1
Diamond Mubaarak <diamondobama@gmail.com>
2024-02-03 21:38:33 +0200

    Adds more tests for 100% code coverage

--- tests/FacadeTest.php
+++ tests/FacadeTest.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace Intervention\Image\Laravel\Tests;
 
+use Error;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Interfaces\ImageInterface;
+use Intervention\Image\Laravel\Facades\Image;
 use ReflectionClass;
+use TypeError;
+use ValueError;
 
 class FacadeTest extends TestCase
 {
@@ -21,5 +28,50 @@ class FacadeTest extends TestCase
         $method = $reflection->getMethod('getFacadeAccessor');
         $method->setAccessible(true);
         $this->assertSame('image', $method->invoke(null));
+    }
+
+    public function testReadAnImage(): void
+    {
+        Storage::fake('images');
+        $input = UploadedFile::fake()->image('image.jpg');
+        $result = Image::read($input);
+        $this->assertInstanceOf(ImageInterface::class, $result);
+    }
+
+    public function testThrowsExceptionWhenReadingNonExistentImage(): void
+    {
+        $this->expectException(Error::class);
+        $input = 'path/to/non_existent_image.jpg';
+        Image::read(null, $input);
+    }
+
+    public function testCreateAnImage(): void
+    {
+        $width = 200;
+        $height = 200;
+        $result = Image::create($width, $height);
+        $this->assertInstanceOf(ImageInterface::class, $result);
+    }
+
+    public function testThrowsExceptionWhenCreatingImageWithInvalidDimensions(): void
+    {
+        $this->expectException(ValueError::class);
+        $width = -200;
+        $height = 200;
+        Image::create($width, $height);
+    }
+
+    public function testAnimateAnImage(): void
+    {
+        $callback = function () {};
+        $result = Image::animate($callback);
+        $this->assertInstanceOf(ImageInterface::class, $result);
+    }
+
+    public function testThrowsExceptionWhenAnimatingImageWithInvalidCallback(): void
+    {
+        $this->expectException(TypeError::class);
+        $callback = 'not_callable';
+        Image::animate($callback);
     }
 }
