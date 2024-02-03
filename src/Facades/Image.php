2d3559365ce14a6349058b1226e20225b926dabc
Diamond Mubaarak <diamondobama@gmail.com>
2024-02-03 23:02:58 +0200

    The facade methods should return ImageInterface and not ImageManager

--- src/Facades/Image.php
+++ src/Facades/Image.php
@@ -7,14 +7,13 @@ namespace Intervention\Image\Laravel\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static \Intervention\Image\ImageManager read(mixed $input, string|array|\Intervention\Image\Interfaces\DecoderInterface $decoders = [])
- * @method static \Intervention\Image\ImageManager create(int $width, int $height)
- * @method static \Intervention\Image\ImageManager animate(callable $callback)
+ * @method static \Intervention\Image\Interfaces\ImageInterface read(mixed $input, string|array|\Intervention\Image\Interfaces\DecoderInterface $decoders = [])
+ * @method static \Intervention\Image\Interfaces\ImageInterface create(int $width, int $height)
+ * @method static \Intervention\Image\Interfaces\ImageInterface animate(callable $callback)
  */
 class Image extends Facade
 {
     protected static function getFacadeAccessor(): string
-
     {
         return 'image';
     }
