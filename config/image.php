4c6dbe2adc90eab6c79aadf1d8fbd1af7d741bf7
Oliver Vogel <oliver@olivervogel.com>
2024-05-25 09:27:16 +0200

    Update comment blocks

--- config/image.php
+++ config/image.php
@@ -23,15 +23,15 @@ return [
     | Configuration Options
     |--------------------------------------------------------------------------
     |
-    | These options control the default behavior of Intervention Image.
+    | These options control the behavior of Intervention Image.
     |
-    | - `autoOrientation` controls whether an imported image should be
+    | - "autoOrientation" controls whether an imported image should be
     |    automatically rotated according to any existing Exif data.
     |
-    | - `decodeAnimation` decides whether a possibly animated image is
+    | - "decodeAnimation" decides whether a possibly animated image is
     |    decoded as such or whether the animation is discarded.
     |
-    | - `blendingColor` Defines the default blending color.
+    | - "blendingColor" Defines the default blending color.
     */
 
     'options' => [
