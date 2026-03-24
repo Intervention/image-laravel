a760b041e5133fd81509414f4955c93ffefb4a7b
Caleb White <cdwhite3@pm.me>
2026-03-24 10:10:30 -0500

    fix: larastan unable to recognize macro (#30)

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Intervention\Image\Laravel;
 
-use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Support\Facades\Response as ResponseFacade;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use Intervention\Image\ImageManager;
@@ -28,17 +27,19 @@ class ServiceProvider extends BaseServiceProvider
             __DIR__ . '/../config/image.php' => config_path(Facades\Image::BINDING . '.php')
         ]);
 
-        $this->app->afterResolving(ResponseFactory::class, function (): void {
-            // register response macro "image"
-            if ($this->shouldCreateResponseMacro()) {
-                ResponseFacade::macro(Facades\Image::BINDING, function (
-                    ImageInterface $image,
-                    null|string|Format|MediaType|FileExtension $format = null,
-                    mixed ...$options,
-                ): Response {
-                    return ImageResponseFactory::make($image, $format, ...$options);
-                });
+        // register response macro "image"
+        ResponseFacade::resolved(function ($factory): void {
+            if ($factory::hasMacro(Facades\Image::BINDING)) {
+                return;
             }
+
+            $factory::macro(Facades\Image::BINDING, function (
+                ImageInterface $image,
+                null|string|Format|MediaType|FileExtension $format = null,
+                mixed ...$options,
+            ): Response {
+                return ImageResponseFactory::make($image, $format, ...$options);
+            });
         });
     }
 
@@ -63,17 +64,5 @@ class ServiceProvider extends BaseServiceProvider
                 strip: config('image.options.strip', false)
             );
         });
-    }
-
-    /**
-     * Determine if response macro should be created
-     */
-    private function shouldCreateResponseMacro(): bool
-    {
-        if (!$this->app->runningUnitTests() && $this->app->runningInConsole()) {
-            return false;
-        }
-
-        return !ResponseFacade::hasMacro(Facades\Image::BINDING);
     }
 }
